394ea4ed73d00a1f7e99d84cf0abf5433b50816a
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-06-08 18:31:03 +0200

    mt76: usb: backport sg_init_marker routine

--- mt76.h
+++ mt76.h
@@ -670,4 +670,24 @@ void mt76u_mcu_complete_urb(struct urb *urb);
 int mt76u_mcu_init_rx(struct mt76_dev *dev);
 void mt76u_mcu_deinit(struct mt76_dev *dev);
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,17,0)
+/**
+ * sg_init_marker - Initialize markers in sg table
+ * @sgl:	   The SG table
+ * @nents:	   Number of entries in table
+ *
+ **/
+static inline void sg_init_marker(struct scatterlist *sgl,
+				  unsigned int nents)
+{
+#ifdef CONFIG_DEBUG_SG
+	unsigned int i;
+
+	for (i = 0; i < nents; i++)
+		sgl[i].sg_magic = SG_MAGIC;
+#endif
+	sg_mark_end(&sgl[nents - 1]);
+}
+#endif /* LINUX_VERSION_CODE < KERNEL_VERSION(4,17,0) */
+
 #endif
