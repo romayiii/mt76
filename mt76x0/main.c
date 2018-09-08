LorenzoBianconi/wireless-drivers-next

mt76: unify conf_tx
Use one conf_tx implementation in mt76x0 and mt76x2. Note this change
conf_tx for mt76x0, but it should work with mt76x2 version.

Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

drivers/net/wireless/mediatek/mt76/mt76x0/main.c
@@ -228,7 +228,7 @@ const struct ieee80211_ops mt76x0_ops = {
 	.sta_add = mt76xx_sta_add,
 	.sta_remove = mt76xx_sta_remove,
 	.set_key = mt76xx_set_key,
-	.conf_tx = mt76x0_conf_tx,
+	.conf_tx = mt76xx_conf_tx,
 	.sw_scan_start = mt76x0_sw_scan,
 	.sw_scan_complete = mt76x0_sw_scan_complete,
 	.ampdu_action = mt76xx_ampdu_action,

drivers/net/wireless/mediatek/mt76/mt76x2_main.c
@@ -306,7 +306,7 @@ const struct ieee80211_ops mt76x2_ops = {
 	.sta_add = mt76xx_sta_add,
 	.sta_remove = mt76xx_sta_remove,
 	.set_key = mt76xx_set_key,
-	.conf_tx = mt76x2_conf_tx,
+	.conf_tx = mt76xx_conf_tx,
 	.sw_scan_start = mt76x2_sw_scan,
 	.sw_scan_complete = mt76x2_sw_scan_complete,
 	.flush = mt76x2_flush,

drivers/net/wireless/mediatek/mt76/mt76x2u_main.c
@@ -173,7 +173,7 @@ const struct ieee80211_ops mt76x2u_ops = {
 	.wake_tx_queue = mt76_wake_tx_queue,
 	.bss_info_changed = mt76x2u_bss_info_changed,
 	.configure_filter = mt76xx_configure_filter,
-	.conf_tx = mt76x2_conf_tx,
+	.conf_tx = mt76xx_conf_tx,
 	.sw_scan_start = mt76x2u_sw_scan,
 	.sw_scan_complete = mt76x2u_sw_scan_complete,
 	.sta_rate_tbl_update = mt76x2_sta_rate_tbl_update,

romayiii/mt76

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>

mt76x0/main.c
@@ -219,7 +219,7 @@ const struct ieee80211_ops mt76x0_ops = {
 	.sta_add = mt76x02_sta_add,
 	.sta_remove = mt76x02_sta_remove,
 	.set_key = mt76x02_set_key,
-	.conf_tx = mt76x0_conf_tx,
+	.conf_tx = mt76x02_conf_tx,
 	.sw_scan_start = mt76x0_sw_scan,
 	.sw_scan_complete = mt76x0_sw_scan_complete,
 	.ampdu_action = mt76x02_ampdu_action,

mt76x2_main.c
@@ -321,7 +321,7 @@ const struct ieee80211_ops mt76x2_ops = {
 	.sta_add = mt76x02_sta_add,
 	.sta_remove = mt76x02_sta_remove,
 	.set_key = mt76x02_set_key,
-	.conf_tx = mt76x2_conf_tx,
+	.conf_tx = mt76x02_conf_tx,
 	.sw_scan_start = mt76x2_sw_scan,
 	.sw_scan_complete = mt76x2_sw_scan_complete,
 	.flush = mt76x2_flush,

mt76x2u_main.c
@@ -173,7 +173,7 @@ const struct ieee80211_ops mt76x2u_ops = {
 	.wake_tx_queue = mt76_wake_tx_queue,
 	.bss_info_changed = mt76x2u_bss_info_changed,
 	.configure_filter = mt76x02_configure_filter,
-	.conf_tx = mt76x2_conf_tx,
+	.conf_tx = mt76x02_conf_tx,
 	.sw_scan_start = mt76x2u_sw_scan,
 	.sw_scan_complete = mt76x2u_sw_scan_complete,
 	.sta_rate_tbl_update = mt76x2_sta_rate_tbl_update,