LorenzoBianconi/wireless-drivers-next

mt76x0: simplify mt76_mac_process_rx signature
Remove data pointer from mt76_mac_process_rx routine signature

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

drivers/net/wireless/mediatek/mt76/mt76x0/dma.c
@@ -44,7 +44,7 @@ mt76x0_rx_skb_from_seg(struct mt76x0_dev *dev, struct mt76xx_rxwi *rxwi,
 	if (!skb)
 		return NULL;
 
-	true_len = mt76_mac_process_rx(dev, skb, data, rxwi);
+	true_len = mt76_mac_process_rx(dev, skb, rxwi);
 	if (!true_len || true_len > seg_len)
 		goto bad_frame;
 

romayiii/mt76

mt76x0/dma.c
@@ -44,7 +44,7 @@ mt76x0_rx_skb_from_seg(struct mt76x0_dev *dev, struct mt76x02_rxwi *rxwi,
 	if (!skb)
 		return NULL;
 
-	true_len = mt76x0_mac_process_rx(dev, skb, data, rxwi);
+	true_len = mt76x0_mac_process_rx(dev, skb, rxwi);
 	if (!true_len || true_len > seg_len)
 		goto bad_frame;
 