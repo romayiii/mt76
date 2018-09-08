LorenzoBianconi/wireless-drivers-next

mt76: unify remove_interface
Use common remove_interface callback in mt76x0 and mt76x2.

Signed-off-by: Stanislaw Gruszka <sgruszka@redhat.com>

drivers/net/wireless/mediatek/mt76/mt76x0/main.c
@@ -62,14 +62,6 @@ static int mt76x0_add_interface(struct ieee80211_hw *hw,
 	return 0;
 }
 
-static void mt76x0_remove_interface(struct ieee80211_hw *hw,
-				     struct ieee80211_vif *vif)
-{
-	struct mt76x0_dev *dev = hw->priv;
-
-	mt76_txq_remove(&dev->mt76, vif->txq);
-}
-
 static int mt76x0_config(struct ieee80211_hw *hw, u32 changed)
 {
 	struct mt76x0_dev *dev = hw->priv;
@@ -212,7 +204,7 @@ const struct ieee80211_ops mt76x0_ops = {
 	.start = mt76x0_start,
 	.stop = mt76x0_stop,
 	.add_interface = mt76x0_add_interface,
-	.remove_interface = mt76x0_remove_interface,
+	.remove_interface = mt76xx_remove_interface,
 	.config = mt76x0_config,
 	.configure_filter = mt76xx_configure_filter,
 	.bss_info_changed = mt76x0_bss_info_changed,

drivers/net/wireless/mediatek/mt76/mt76x2_main.c
@@ -299,7 +299,7 @@ const struct ieee80211_ops mt76x2_ops = {
 	.start = mt76x2_start,
 	.stop = mt76x2_stop,
 	.add_interface = mt76x2_add_interface,
-	.remove_interface = mt76x2_remove_interface,
+	.remove_interface = mt76xx_remove_interface,
 	.config = mt76x2_config,
 	.configure_filter = mt76xx_configure_filter,
 	.bss_info_changed = mt76x2_bss_info_changed,

drivers/net/wireless/mediatek/mt76/mt76x2u_main.c
@@ -164,7 +164,7 @@ const struct ieee80211_ops mt76x2u_ops = {
 	.start = mt76x2u_start,
 	.stop = mt76x2u_stop,
 	.add_interface = mt76x2u_add_interface,
-	.remove_interface = mt76x2_remove_interface,
+	.remove_interface = mt76xx_remove_interface,
 	.sta_add = mt76xx_sta_add,
 	.sta_remove = mt76xx_sta_remove,
 	.set_key = mt76xx_set_key,

romayiii/mt76

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>

mt76x0/main.c
@@ -62,14 +62,6 @@ static int mt76x0_add_interface(struct ieee80211_hw *hw,
 	return 0;
 }
 
-static void mt76x0_remove_interface(struct ieee80211_hw *hw,
-				     struct ieee80211_vif *vif)
-{
-	struct mt76x0_dev *dev = hw->priv;
-
-	mt76_txq_remove(&dev->mt76, vif->txq);
-}
-
 static int mt76x0_config(struct ieee80211_hw *hw, u32 changed)
 {
 	struct mt76x0_dev *dev = hw->priv;
@@ -203,7 +195,7 @@ const struct ieee80211_ops mt76x0_ops = {
 	.start = mt76x0_start,
 	.stop = mt76x0_stop,
 	.add_interface = mt76x0_add_interface,
-	.remove_interface = mt76x0_remove_interface,
+	.remove_interface = mt76x02_remove_interface,
 	.config = mt76x0_config,
 	.configure_filter = mt76x02_configure_filter,
 	.bss_info_changed = mt76x0_bss_info_changed,

mt76x2_main.c
@@ -314,7 +314,7 @@ const struct ieee80211_ops mt76x2_ops = {
 	.start = mt76x2_start,
 	.stop = mt76x2_stop,
 	.add_interface = mt76x2_add_interface,
-	.remove_interface = mt76x2_remove_interface,
+	.remove_interface = mt76x02_remove_interface,
 	.config = mt76x2_config,
 	.configure_filter = mt76x02_configure_filter,
 	.bss_info_changed = mt76x2_bss_info_changed,

mt76x2u_main.c
@@ -164,7 +164,7 @@ const struct ieee80211_ops mt76x2u_ops = {
 	.start = mt76x2u_start,
 	.stop = mt76x2u_stop,
 	.add_interface = mt76x2u_add_interface,
-	.remove_interface = mt76x2_remove_interface,
+	.remove_interface = mt76x02_remove_interface,
 	.sta_add = mt76x02_sta_add,
 	.sta_remove = mt76x02_sta_remove,
 	.set_key = mt76x02_set_key,