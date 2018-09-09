2dbff477ed91fd212d860c4738a2d4f295ad4636
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-09-09 15:49:37 +0200

    mt76x0: remove unused usb header file
    Remove unused usb header file and move mt76x0 firmware definition
    in usb.c
    
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>

--- mt76x0/usb.c
+++ mt76x0/usb.c
@@ -17,11 +17,12 @@
 #include <linux/usb.h>
 
 #include "mt76x0.h"
-#include "usb.h"
 #include "mcu.h"
 #include "trace.h"
 #include "../mt76x02_util.h"
 #include "../mt76x02_usb.h"
+
+#define MT7610_FIRMWARE			"mediatek/mt7610u.bin"
 
 static struct usb_device_id mt76x0_device_table[] = {
 	{ USB_DEVICE(0x148F, 0x7610) },	/* MT7610U */
