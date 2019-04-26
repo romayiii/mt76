f044f4071c63f23c7f27dde320d8adc66cad3a12
Lorenzo Bianconi <lorenzo@kernel.org>
2019-04-26 15:31:46 +0200

    mt76: move pre_tbtt_tasklet in mt76_dev
    Move pre_tbtt_tasklet tasklet in mt76_dev data structure since
    it is used by all drivers
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7603/mt7603.h
+++ mt7603/mt7603.h
@@ -143,7 +143,6 @@ struct mt7603_dev {
 	unsigned int reset_cause[__RESET_CAUSE_MAX];
 
 	struct delayed_work mac_work;
-	struct tasklet_struct pre_tbtt_tasklet;
 };
 
 extern const struct mt76_driver_ops mt7603_drv_ops;
