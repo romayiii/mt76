7fff5a34924bba6ebdc9fa75e8772ac5dda1fb4d
Lorenzo Bianconi <lorenzo@kernel.org>
2020-04-06 11:07:46 +0200

    mt76: mt7615: introduce PM support
    Introduce suspend/resume to mt7615e driver
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7615/pci.c
+++ mt7615/pci.c
@@ -55,11 +55,49 @@ static void mt7615_pci_remove(struct pci_dev *pdev)
 	pci_free_irq_vectors(pdev);
 }
 
+static int __maybe_unused mt7615_pci_suspend(struct pci_dev *pdev,
+					     pm_message_t state)
+{
+	struct mt76_dev *mdev = pci_get_drvdata(pdev);
+	struct mt7615_dev *dev = container_of(mdev, struct mt7615_dev, mt76);
+	int i;
+
+	napi_disable(&mdev->tx_napi);
+	for (i = 0; i < ARRAY_SIZE(mdev->q_tx); i++)
+		mt76_queue_tx_cleanup(dev, i, true);
+	tasklet_kill(&mdev->tx_tasklet);
+
+	for (i = 0; i < ARRAY_SIZE(mdev->q_rx); i++) {
+		napi_disable(&mdev->napi[i]);
+		mt76_queue_rx_reset(dev, i);
+	}
+
+	return 0;
+}
+
+static int __maybe_unused mt7615_pci_resume(struct pci_dev *pdev)
+{
+	struct mt76_dev *mdev = pci_get_drvdata(pdev);
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(mdev->q_rx); i++)
+		napi_enable(&mdev->napi[i]);
+
+	napi_enable(&mdev->tx_napi);
+
+	return 0;
+}
+
+
 struct pci_driver mt7615_pci_driver = {
 	.name		= KBUILD_MODNAME,
 	.id_table	= mt7615_pci_device_table,
 	.probe		= mt7615_pci_probe,
 	.remove		= mt7615_pci_remove,
+#ifdef CONFIG_PM
+	.suspend 	= mt7615_pci_suspend,
+	.resume 	= mt7615_pci_resume,
+#endif /* CONFIG_PM */
 };
 
 MODULE_DEVICE_TABLE(pci, mt7615_pci_device_table);
